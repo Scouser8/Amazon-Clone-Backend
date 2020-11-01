220ab893eb176345c2e30ba94958e0f80bad23af
Muhammad Abd Al-Baqi <45911940+Scouser8@users.noreply.github.com>
2020-11-01 21:36:00 +0200

    Modified CORS to public for all

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const stripe = require("stripe")(
   "sk_test_51Hi2tjF2rdrsGf19nMDWZSczmx4ZG8RXCPBtOE4v5tKy8cZshqIvqs0hb5dNhKua3jxUq8EWDOOkRI7JIh0Pt8e8009eFdSI3p"
 );
@@ -12,7 +12,15 @@ const port = process.env.PORT || 7000;
 app.listen(port, () => console.log(`Server running locally on port ${port}`));
 
 //Middlewares
-app.use(cors());
+// app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 
 // app.use(express.urlencoded({ extended: false }));
